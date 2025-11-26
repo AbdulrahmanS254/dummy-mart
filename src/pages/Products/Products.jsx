8776d12cf324dd7637c0d61162eb149198b7679c
Abdulrahman Shawky <hawk18922@gmail.com>
2025-11-26 22:46:06 +0200

    style: edited products page (not finished)

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,7 +1,13 @@
 export default function Products() {
     return (
         <main>
-            <div></div>
+            <div className="container mx-auto px-10 lg:px-25">
+                <div className="grid grid-cols-4 gap-4">
+                    <div className="card">
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </div>
         </main>
     );
 }
