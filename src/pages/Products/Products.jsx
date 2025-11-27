6e70da1f44de2db3900f2c684b30120ea0bf3e5c
Abdulrahman Shawky <hawk18922@gmail.com>
2025-11-27 16:49:52 +0200

    edit: products page

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,13 +1,28 @@
 export default function Products() {
+    const source =
+        "https://static.wikia.nocookie.net/fortnite/images/c/c9/Dummy_-_Outfit_-_Fortnite.png/revision/latest?cb=20200721083946";
     return (
-        <main>
+        <section className="products-section py-12 md:py-24">
             <div className="container mx-auto px-10 lg:px-25">
                 <div className="grid grid-cols-4 gap-4">
-                    <div className="card">
-                        <img src="" alt="" />
+                    <div className="product-card bg-orange-50 p-5 rounded shadow-sm flex flex-col ">
+                        <div className="image-wrapper">
+                            <img src={source} alt="" />
+                        </div>
+                        <div className="card-text">
+                            <h3 className="title text-2xl">
+                                Some title
+                            </h3>
+                            <p className="description text-sm">
+                                some description some description some description some description some description some description
+                            </p>
+                        </div>
+                        <div className="card-footer">
+                            <button>Add to cart</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </main>
+        </section>
     );
 }
